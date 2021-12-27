0bdef96ddca002d442d63d80a88f3868eb8f7180
artemned <44065333+artemned@users.noreply.github.com>
2021-12-27 12:35:38 +0300

    Add files via upload

--- Night light/Nigth_ligth_demo/Nigth_ligth_demo.ino
+++ Night light/Nigth_ligth_demo/Nigth_ligth_demo.ino
@@ -12,6 +12,8 @@
 #define PWM_TWO PB0
 analog_pin_t myPin = PIN_DIMMER;
 
+int8_t count_pressed=0;
+
 // state buttons
 bool current_button_on_off = false;
 bool last_button_on_off = false;
@@ -45,23 +47,41 @@ bool pressed_button(bool last,int pin)
     return current;
 }
 
- void configurate_current_state(state_nigth_light on_off)
+ void configurate_current_state(state_nigth_light on_off)// on or off night light
  {
     switch(on_off)
     {
       case Off:digitalWrite(PWM_ONE,LOW);digitalWrite(PWM_TWO,LOW);
       break;
-      case On:digitalWrite(PWM_ONE,digitalRead(myPin)/4);digitalWrite(PWM_TWO,digitalRead(myPin)/4);
+      case On:digitalWrite(PWM_ONE,HIGH);digitalWrite(PWM_TWO,HIGH);
       break;
+      default: break;
     } 
   
  }
+ 
+ void toggling_mode_nigth_light(mode_nigth_light mode_)
+ {
+      switch(mode_)
+      {
+        case Left_light:digitalWrite(PWM_ONE,digitalRead(myPin)/4);digitalWrite(PWM_TWO,LOW);
+        break;
+        case Right_light:digitalWrite(PWM_ONE,LOW);digitalWrite(PWM_TWO,digitalRead(myPin)/4);
+        break;
+        case Both_lights:digitalWrite(PWM_ONE,digitalRead(myPin)/4);digitalWrite(PWM_TWO,digitalRead(myPin)/4);  
+        break;
+        default:break;
+      
+      }  
+ }
 
+ state_nigth_light state = state_nigth_light::On; //start state
+ mode_nigth_light mode = mode_nigth_light::Left_light;
 
 //setup pins
 void setup()
 {
-    state_nigth_light state=state_nigth_light::On;       
+          
     pinMode(PIN_BUTTON_MODE,INPUT_PULLUP); 
     pinMode(PIN_BUTTON_ON_OFF,INPUT_PULLUP);
     pinMode(PIN_DIMMER,INPUT);
@@ -75,12 +95,22 @@ void loop()
  
   current_button_on_off = pressed_button(last_button_on_off,PIN_BUTTON_ON_OFF);
   current_button_mode = pressed_button(last_button_mode,PIN_BUTTON_MODE);
+  
   if(last_button_on_off==false && current_button_on_off==true)
   {   
       configurate_current_state(state);      
-  }  
-  
+  }   
   last_button_on_off = current_button_on_off;
- 
+  state!=state;
+  
+  if(last_button_mode==false && current_button_mode==true)
+  {
+    toggling_mode_nigth_light(mode);
+    count_pressed++;
     
+  }
+  last_button_mode=current_button_mode;
+  if(count_pressed>2)count_pressed=0;
+  mode=static_cast<mode_nigth_light>(count_pressed);
+     
 }
